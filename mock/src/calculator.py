69a5114968ee8783596a2aaf3b1bb32e70ff6399
Ezequiel Seibert Hellwig <104174080+ezequielshell@users.noreply.github.com>
2023-04-27 21:08:49 -0300

    Add files via upload

---
+++ mock/src/calculator.py
@@ -0,0 +1,8 @@
+class Calculator:
+
+    def add(self, a, b):
+        return a + b
+
+
+    def substract(self, a, b):
+        return a - b
---
+++ mock/tests/test.calculator.py
@@ -0,0 +1,14 @@
+import pytest
+from src.calculator import Calculator
+
+
+def test_add(mocker):
+    mocker.patch("src.Calculator.Calculator.add", return_value=5)
+    calculator = Calculator()
+    assert calculator.add(2, 3) == 5
+
+    
+    
+
+    
+
